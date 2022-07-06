3eecb8f909ccb8d6bd0469e6b3928c55f5b48589
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-06 18:58:15 +0930

    Update ft_strchr.c

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -12,7 +12,19 @@ Return value:
 */
 
 char    *ft_strchr(const char *str, int c)
-{
-
+{   
+    int i;
+    
+    while(str[i] != '\0')
+    {
+        if (str[i] == c)
+        {
+            return ((char *)str[i]);
+            i++;
+        }
+    }
+    if (c == '\0')
+        return ((char *)str[i]);
+    return (0);
 }
 
