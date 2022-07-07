8f72c95af57818356a52de0d3d45837337511fa3
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-07 22:59:48 +0930

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -20,10 +20,24 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int ft_atoi(const char *str);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+int ft_strncmp(const char *str1, const char *str2, size_t n);
+int ft_tolower(int c);
+int ft_toupper(int c);
+
 size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
+size_t  ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);
+size_t  ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
+
 void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void  *ft_memchr(const void *s, int c, size_t n)
+
+char  *ft_strchr(const char *str, int c);
+char  *ft_strnstr(const char *str, const char *pattern, size_t n);
+char  *ft_strrchr(const char *s, int c);
 
 #endif
