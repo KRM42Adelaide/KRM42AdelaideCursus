e5b13ffcdd6837be65aeffff41637468b5fa0dd0
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-07 20:08:21 +0930

    Update ft_memchr.c

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,18 @@ ft_memchr
 
 This function returns a pointer to the matching byte or NULL
 */
-void    *ft_memchr(const void *str, int c, size_t n)
+void    *ft_memchr(const void *s, int c, size_t n)
 {
-
+    const char  *str;
+    size_t  i;
+    
+    str = (const char *)s;
+    i = 0;
+    while (i < n)
+    {
+        if (str[i] == c)
+            return ((void *)(s + 1)):
+        i++;
+    }
+    return (0);
 }
