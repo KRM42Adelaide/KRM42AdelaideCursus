38709bdbafccf28de92dfd17c0611ccb13b0f1b4
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 16:12:43 +0930

    ft_strmapi
    Applies the function ’f’ to each character of the
    string ’s’, and passing its index as first argument
    to create a new string (with malloc(3)) resulting
    from successive applications of ’f’.

---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,26 @@
+/*
+** LIBRARY: N/A
+** SYNOPSIS: create new string from modifying string with specified function
+**
+** DESCRIPTION:
+** 		Applies the function ’f’ to each character of the string ’s’ to create
+**	a new string (with malloc(3)) resulting from successive applications of ’f’.
+*/
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	if (!s || !f || !(str = ft_strdup(s)))
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		str[i] = f(i, str[i]);
+		i++;
+	}
+	return (str);
+}
