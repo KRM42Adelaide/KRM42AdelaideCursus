4a916e12cd719a17d7f0de7154fbbe999788c3c8
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 18:50:00 +0930

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -1,5 +1,10 @@
 #include "libft.h"
 
+/*
+The C library function int memcmp(const void *str1, const void *str2, size_t n) 
+compares the first n bytes of memory area str1 and memory area str2.
+*/
+
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
   size_t  i;
