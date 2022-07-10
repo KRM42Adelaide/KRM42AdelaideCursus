b597e0bc0d5db7dc01f3b9024b0da39062ab5e4f
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 18:33:54 +0930

    Update ft_atoi.c

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -11,10 +11,10 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 /*
 ft_atoi converts the string argument str to an interger
     -> str is the string representation of an integral number
-
 Return Value:
     -> returns the coverted integral number as an int value
     -> If no valid conversion could be performed, it returns zero
