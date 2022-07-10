7dda132eb6ae7401dc5be0022b76007b4d86f84f
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 19:22:28 +0930

    Update ft_strchr.c

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -1,11 +1,10 @@
-#include <stddef.h>
-#include <stdio.h>
+#include "libft.h"
+
 /*
 ft_strchr searches for the first occurrence of the character c (an 
 unsigned char) in the string pointed to by the argument str.
     -> str is the C string to be scanned
     -> c is the character to be searched in str
-
 Return value:
     -> returns a POINTER to the first occurence of the characater c in the string
     -> returns NULL if the character is not found
@@ -27,4 +26,3 @@ char    *ft_strchr(const char *str, int c)
         return ((char *)str[i]);
     return (0);
 }
-
