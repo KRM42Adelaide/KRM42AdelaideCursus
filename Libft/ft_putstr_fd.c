c9a12dc6458a13da2797f685028fc4a770584a7e
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 16:28:07 +0930

    ft_putstr_fd.c
    Outputs the string ’s’ to the given file descriptor.

---
+++ Libft/ft_putstr_fd.c
@@ -0,0 +1,24 @@
+/*
+** LIBRARY: N/A
+** SYNOPSIS: output string to given file
+**
+** DESCRIPTION:
+** 		Outputs the string ’s’ to the given file descriptor.
+*/
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int i;
+
+	if (s != NULL)
+	{
+		i = 0;
+		while (s[i])
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
+	}
+}
