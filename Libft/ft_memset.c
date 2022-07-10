9982c23d0fc842e139414e7edcb8e1e9d8e1ee45
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 18:57:03 +0930

    Update ft_memset.c

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -10,7 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
+
+/*
+The C library function void *memset(void *str, int c, size_t n) 
+copies the character c (an unsigned char) to the first n 
+characters of the string pointed to, by the argument str.
+*/
 
 void	*ft_memset(void *b, int c, size_t len)
 {
@@ -24,6 +30,3 @@ void	*ft_memset(void *b, int c, size_t len)
 	}
 	return (b);
 }
-/*The C library function void *memset(void *str, int c, size_t n) 
-copies the character c (an unsigned char) to the first n 
-characters of the string pointed to, by the argument str.*/
