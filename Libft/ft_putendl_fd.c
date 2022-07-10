ab350c17d37f938a5ac14a583b10b5c03e69c569
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 16:39:06 +0930

    ft_putendl_fd.c
    Outputs the string ’s’ to the given file descriptor, followed by a newline.

---
+++ Libft/ft_putendl_fd.c
@@ -0,0 +1,19 @@
+/*
+** LIBRARY: N/A
+** SYNOPSIS: output string to given file with newline
+**
+** DESCRIPTION:
+** 		Outputs the string ’s’ to the given file descriptor, followed by a
+**	newline.
+*/
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (s != NULL)
+	{
+		ft_putstr_fd(s, fd);
+		ft_putchar_fd('\n', fd);
+	}
+}
