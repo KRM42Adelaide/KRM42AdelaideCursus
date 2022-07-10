a448af67571fde21460d77514b135d579679f48a
KRM42Adelaide <106360363+KRM42Adelaide@users.noreply.github.com>
2022-07-10 16:08:42 +0930

    ft_itoa
    Allocates (with malloc(3)) and returns a string representing the integer received as an argument. Negative numbers must be handled.

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,39 @@
+/*
+** LIBRARY: N/A
+** SYNOPSIS: convert integer to ASCII string
+**
+** DESCRIPTION:
+** 		Allocates (with malloc(3)) and returns a string representing the
+**	integer received as an argument. Negative numbers must be handled.
+*/
+
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	long	nbr;
+	size_t	size;
+
+	nbr = n;
+	size = n > 0 ? 0 : 1;
+	nbr = nbr > 0 ? nbr : -nbr;
+	while (n)
+	{
+		n /= 10;
+		size++;
+	}
+	if (!(str = (char *)malloc(size + 1)))
+		return (0);
+	*(str + size--) = '\0';
+	while (nbr > 0)
+	{
+		*(str + size--) = nbr % 10 + '0';
+		nbr /= 10;
+	}
+	if (size == 0 && str[1] == '\0')
+		*(str + size) = '0';
+	else if (size == 0 && str[1] != '\0')
+		*(str + size) = '-';
+	return (str);
+}
